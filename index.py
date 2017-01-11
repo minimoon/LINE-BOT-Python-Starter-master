1f32eb9f8bec045a7ac634ba42237f26a7443e82
minimoon <sasivimoon@gmail.com>
2017-01-11 09:00:35 +0700

    Update index.py

--- index.py
+++ index.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
      app.run(debug=True)
   
 '''
+https://python3.wannaphong.com/2016/10/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-line-bot-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-python.html
 {
   "events": [
       {
